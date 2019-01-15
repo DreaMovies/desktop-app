0f485f4bb8f62504bc893f0b5dd4cdcce572eeee
SrPatinhas <srpatinhas>
2019-01-15 17:43:11 +0000

    intial commit for travis build test

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'dreamovies_app_alpha'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
