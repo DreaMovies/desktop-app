978f23fe8e0d8b2c91627ac55c5c158ed588e420
SrPatinhas <srpatinhas>
2019-01-21 17:49:08 +0000

    fix plugins list

--- src/renderer/components/layout/sidebar.vue
+++ src/renderer/components/layout/sidebar.vue
@@ -1,40 +1,42 @@
 <template>
-	<div>
-		<ul :class="['navbar-nav side-nav', (expanded ? 'nav-open' : 'nav-closed')]">
-			<li class="nav-item">
-				<!-- literal string -->
-				<b-link class="nav-link" to="/index">Index</b-link>
-			</li>
-			<li class="nav-item">
-				<b-link class="nav-link" to="/local-media">Local Media</b-link>
-			</li>
-			<li class="nav-item">
-				<b-link class="nav-link disabled">API's</b-link>
-			</li>
-			<li class="nav-item">
-				<b-link class="nav-link" to="/movie-list/dreamovies">Dreamovies</b-link>
-			</li>
-			<li class="nav-item">
-				<b-link class="nav-link" to="/movie-list/eztv">EZTV</b-link>
-			</li>
-			<li class="nav-item">
-				<b-link class="nav-link" to="/movie-list/yts">YTS</b-link>
-			</li>
-		</ul>
-		<listPlugins></listPlugins>
-	</div>
+	<ul :class="['navbar-nav side-nav', (expanded ? 'nav-open' : 'nav-closed')]">
+		<li class="nav-item">
+			<!-- literal string -->
+			<b-link class="nav-link" to="/index">Index</b-link>
+		</li>
+		<li class="nav-item">
+			<b-link class="nav-link" to="/local-media">Local Media</b-link>
+		</li>
+		<li class="nav-item">
+			<b-link class="nav-link disabled">API's</b-link>
+		</li>
+		<li class="nav-item">
+			<b-link class="nav-link" to="/movie-list/dreamovies">Dreamovies</b-link>
+		</li>
+		<li class="nav-item">
+			<b-link class="nav-link" to="/movie-list/eztv">EZTV</b-link>
+		</li>
+		<li class="nav-item">
+			<b-link class="nav-link" to="/movie-list/yts">YTS</b-link>
+		</li>
+		<b-nav-item v-for="item in list">{{ item.config.label }}</b-nav-item>
+	</ul>
 </template>
 
 <script>
-	import listPlugins from '../elements/list_plugins.vue';
 	export default {
 		name: 'layout-sidebar',
 		props: [
 			'expanded'
 		],
-        components: {
-            listPlugins,
-        },
+		data: function(){
+			return {
+				list: []
+			}
+		},
+        created() {
+			this.list = this.$plugins;
+		},
 		methods: {
 			// open(link) {
 			// this.link = link;
